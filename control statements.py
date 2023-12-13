60acfc8eafa1f1a60a8a3207897b20f1653ab222
durgaraobattula <153579813+durgaraobattula@users.noreply.github.com>
2023-12-13 15:03:55 +0530

    Add files via upload

---
+++ control statements.py
@@ -0,0 +1,8 @@
+a = int(input("enter a value "))
+b = int(input("enter b value "))
+if a>b:
+    print("a is greater than b ")
+elif a<b:
+    print("a is less than b ")
+else:
+    print("both values are equal")
