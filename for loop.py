df8fb7ab67bf986cbe4b88454b1c615db2b5f999
durgaraobattula <153579813+durgaraobattula@users.noreply.github.com>
2023-12-14 12:13:15 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,130 @@
+#fruits = ["apple", "banana","cherry"]
+#for x in fruits:
+ #   print(x)
+
+
+#for x in "apple":
+    #print(x)
+
+#fruits = ["apple", "banana","cherry"]
+#for x in fruits:
+ #if x == "banana":
+  #continue
+ #print(x)
+
+
+#for x in range(2,7,2):
+    #print(x)
+
+#for x in range(7):
+   #if x == 3: break
+   #print(x)
+#else:
+    #print("finally finished ")
+
+
+#adj = ["red", "big","tasty"]
+#fruits = ["apple","banana","cherry"]
+#for x in adj:
+    #for y in fruits:
+        #print(x,y)
+
+#for x in "apple":
+    #print(x)
+
+#fruits = ["apple","banana","cherry"]
+#for x in fruits:
+    #if x == "banana":
+        #break
+    #print(x)
+
+
+#fruits = ["apple","banana","cheery"]
+#for x in fruits:
+    #if x == "banana":
+        #continue
+    #print(x)
+
+
+
+#for x in range(2,6,2):
+    #print(x)
+
+
+#for x in range(6):
+    #if x == 3:break
+    #print(x)
+#else:
+    #print("finally finished ")
+
+
+#adj =["red","big","tasty"]
+#fruits = ["apple","banana","cherry"]
+#for x in adj:
+    #for y in fruits:
+        #print(x,y)
+
+
+#for x in [0,1,2]:
+    #pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
