4c28cbea51fcbd6b216684f640f2cbef99317f3c
durgaraobattula <153579813+durgaraobattula@users.noreply.github.com>
2023-12-14 12:51:59 +0530

    14DEC

--- for loop.py
+++ for loop.py
@@ -1,72 +1,72 @@
-#fruits = ["apple", "banana","cherry"]
-#for x in fruits:
- #   print(x)
+fruits = ["apple", "banana","cherry"]
+for x in fruits:
+    print(x)
 
 
-#for x in "apple":
-    #print(x)
+for x in "apple":
+    print(x)
 
-#fruits = ["apple", "banana","cherry"]
-#for x in fruits:
- #if x == "banana":
-  #continue
- #print(x)
-
-
-#for x in range(2,7,2):
-    #print(x)
-
-#for x in range(7):
-   #if x == 3: break
-   #print(x)
-#else:
-    #print("finally finished ")
+fruits = ["apple", "banana","cherry"]
+for x in fruits:
+ if x == "banana":
+  continue
+ print(x)
 
 
-#adj = ["red", "big","tasty"]
-#fruits = ["apple","banana","cherry"]
-#for x in adj:
-    #for y in fruits:
-        #print(x,y)
+for x in range(2,7,2):
+    print(x)
 
-#for x in "apple":
-    #print(x)
-
-#fruits = ["apple","banana","cherry"]
-#for x in fruits:
-    #if x == "banana":
-        #break
-    #print(x)
+for x in range(7):
+   if x == 3: break
+   print(x)
+else:
+    print("finally finished ")
 
 
-#fruits = ["apple","banana","cheery"]
-#for x in fruits:
-    #if x == "banana":
-        #continue
-    #print(x)
+adj = ["red", "big","tasty"]
+fruits = ["apple","banana","cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
+
+for x in "apple":
+    print(x)
+
+fruits = ["apple","banana","cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+
+
+fruits = ["apple","banana","cheery"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
 
 
 
-#for x in range(2,6,2):
-    #print(x)
+for x in range(2,6,2):
+    print(x)
 
 
-#for x in range(6):
-    #if x == 3:break
-    #print(x)
-#else:
-    #print("finally finished ")
+for x in range(6):
+    if x == 3:break
+    print(x)
+else:
+    print("finally finished ")
 
 
-#adj =["red","big","tasty"]
-#fruits = ["apple","banana","cherry"]
-#for x in adj:
-    #for y in fruits:
-        #print(x,y)
+adj =["red","big","tasty"]
+fruits = ["apple","banana","cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
 
 
-#for x in [0,1,2]:
-    #pass
+for x in [0,1,2]:
+    pass
 
 
 
